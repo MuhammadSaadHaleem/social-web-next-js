811bd04aca4da44a3d4d4c289a12c55778efb40e
Muhammad Saad Haleem <49789953+MuhammadSaadHaleem@users.noreply.github.com>
2023-12-03 07:01:23 +0500

    Update AddData.js

--- src/app/component/AddData.js
+++ src/app/component/AddData.js
@@ -38,36 +38,35 @@ const AddData = () => {
 
     return (
         <div className="flex gap-4 border-b border-gray-300 pb-4 ">
-            <div>
-            <img className="w-[44px] h-[44px] object-cover rounded-full mt-[20px]" src="./avatar.png" alt="dp" />
+            <div style={{ position: "fixed" }}>
+                <img className="w-[44px] h-[44px] object-cover rounded-full mt-[20px]" src="./avatar.png" alt="dp" />
 
-            <input
-                className="outline-none border-none w-[100%] text-[18px] placeholder:text-gray-600 mt-[20px]"
-                type="text"
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-                placeholder="What's on your mind?"
-            />
+                <input
+                    className="outline-none border-none w-[100%] text-[18px] placeholder:text-gray-600 mt-[20px]"
+                    type="text"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                    placeholder="What's on your mind?"
+                />
 
-            <button
-                onClick={postData}
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            >
-                Post
-            </button>
-            <input className="mt-[20px]" type="file" onChange={(e) => setUpdateFile(e.target.files)} />
+                <button
+                    onClick={postData}
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                >
+                    Post
+                </button>
+                <input className="mt-[20px]" type="file" onChange={(e) => setUpdateFile(e.target.files)} />
 
             </div>
 
-            <div className="outline-none border-none w-[100%] text-[18px] placeholder:text-gray-600 mt-[100px] overflow-y-scroll h-72">
+            <div className="outline-none border-none w-[100%] text-[18px] placeholder:text-gray-600 mt-[200px] overflow-y-scroll h-72">
                 {adpost.map(function (items) {
                     return (
                         <>
-
-
-                            {items.input}
-
-                            <img src={items.imageUrl} style={{width:"600px", height:"400px", alignItems:"center"}}/>
+                            <div className=" mt-[50px]">
+                                {items.input}
+                            </div>
+                            <img src={items.imageUrl} style={{ width: "600px", height: "400px", alignItems: "center" }} />
 
                         </>
                     )
